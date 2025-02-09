eb8458180ac9df3a589f203b66033229a9418747
Alan <alandsantos54@gmail.com>
2025-02-09 16:16:32 -0300

    exportando do servidor

--- scripts.js
+++ scripts.js
@@ -1,33 +1,37 @@
-const convertButton = document.querySelector(".convert-button")
-const currencySelect = document.querySelector(".currency-select")
+const convertButton = document.querySelector(".convert-button");
+const currencySelect = document.querySelector(".currency-select");
 
-function convertValues() {
-    const inputCurrencyValue = document.querySelector(".input-currency").value
-    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
-    const currencyValueConverted = document.querySelector(".currency-value")
+const kwanza = 0.0066286623359406
 
-    const dolarToday = 5.83
-    const euroToday = 6.06
-    const libraToday = 7.24
-    const ieneToday = 0.037037037
-    const kwanzaToday = 0.0062980224
-    const bitcoinToday = 601.812
-    const pesoargToday = 0.0055928412
+const convertValues = async () => {
+    const inputCurrencyValue = document.querySelector(".input-currency").value;
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert");
+    const currencyValueConverted = document.querySelector(".currency-value");
 
+    const data = await fetch(" https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL,JPY-BRL,ARS-BRL,GBP-BRL").then(response => response.json())
+  
+    const dolar = data.USDBRL.high
+    const euro = data.EURBRL.high
+    const bitcoin = data.BTCBRL.high
+    const iene = data.JPYBRL.high
+    const pesoarg = data.ARSBRL.high
+    const libra = data.GBPBRL.high
+
+    
 
     if (currencySelect.value == "pesoarg") {
         currencyValueConverted.innerHTML = new Intl.NumberFormat("es-AR", {
             style: "currency",
             currency: "ARS",
             minimumFractionDigits: 2
-        }).format(inputCurrencyValue / pesoargToday)
+        }).format(inputCurrencyValue / pesoarg);
     }
 
     if (currencySelect.value == "libra") {
         currencyValueConverted.innerHTML = new Intl.NumberFormat("en-GB", {
             style: "currency",
             currency: "GBP"
-        }).format(inputCurrencyValue / libraToday)
+        }).format(inputCurrencyValue / libra);
     }
 
     if (currencySelect.value == "bitcoin") {
@@ -35,14 +39,14 @@ function convertValues() {
             style: "currency",
             currency: "BTC",
             minimumFractionDigits: 8
-        }).format(inputCurrencyValue / bitcoinToday)
+        }).format(inputCurrencyValue / bitcoin);
     }
 
     if (currencySelect.value == "kwanza") {
         currencyValueConverted.innerHTML = new Intl.NumberFormat("pt-AO", {
             style: "currency",
             currency: "AOA"
-        }).format(inputCurrencyValue / kwanzaToday)
+        }).format(inputCurrencyValue / kwanza);
     }
 
 
@@ -50,21 +54,21 @@ function convertValues() {
         currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
             style: "currency",
             currency: "USD"
-        }).format(inputCurrencyValue / dolarToday)
+        }).format(inputCurrencyValue / dolar);
     }
 
     if (currencySelect.value == "euro") {
         currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
             style: "currency",
             currency: "EUR"
-        }).format(inputCurrencyValue / euroToday)
+        }).format(inputCurrencyValue / euro);
     }
 
     if (currencySelect.value == "iene") {
         currencyValueConverted.innerHTML = new Intl.NumberFormat("ja-JP", {
             style: "currency",
             currency: "JPY"
-        }).format(inputCurrencyValue / ieneToday)
+        }).format(inputCurrencyValue / iene);
     }
 
 
